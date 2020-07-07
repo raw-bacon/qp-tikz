ef9744e59923dfc76ce6b60883accef4c3d19ec6
Levi Ryffel <levi.ryffel@gmail.com>
2020-07-07 13:56:05 +0200

    renamed BraidParser.py to parsing.py

--- __main__.py
+++ __main__.py
@@ -1,4 +1,4 @@
-from BraidParser import parse
+from parsing import parse
 from Surface import Surface
 
 print("Please enter a braid, e.g., 1,2/1,3/1,4 for sigma_(1,2) * sigma_(1,3) * sigma_(1, 4).\n")
